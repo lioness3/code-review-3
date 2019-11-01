030ef38f0d730c6702b6d2423ca0b934cc104bc5
Joann Carter <jcnailtech3@yahoo.com>
2019-11-01 14:09:01 -0700

    troubleshoot replace method

--- js/scripts.js
+++ js/scripts.js
@@ -14,19 +14,22 @@ $(document).ready(function(){
 // function generateRange(min,max,step){
 // for (index=min; index<=max index=index+step){
 //   console.log(.push(index));
-for (var index=inputValue; index >=0 ; index-=1)
-$("#visual").prepend(index + ",");
+for (var index=inputValue; index >=0 ; index-=1){
 
-//   if(userAnswer === "3"){
-//  true;
-// }else if(userAnswer === "2"){
-// true;
+  (returnDisplay).push(index + ",");
+if(returnDisplay.includes("3")){
+ returnDisplay.replace(/"3"/g, "I'm sorry, Dave. I'm afraid I can't do that.");
+ console.log(returnDisplay);
+}
+// else if(userAnswer === "2"){
+// var answer2=userAnswer.replace(/2/g,"Boop!")
 // }else if(userAnswer === "1"){
 // true;
-// }else{
-//   console.log("ooops!");
-// };
-
+// }
+else{
+  console.log("ooops!");
+};
+};
 
 
 
