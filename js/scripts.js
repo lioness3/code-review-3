84ea324e53f9f2d6c376a438f5da360977975c03
Joann Carter <jcnailtech3@yahoo.com>
2019-11-01 16:20:20 -0700

    fix indents

--- js/scripts.js
+++ js/scripts.js
@@ -11,18 +11,17 @@ $(document).ready(function(){
     var returnDisplay = [];
 
     for (var index=0;index < parseInt(inputValue)+1;index++){
-      for (var individual = 0;individual < index.toString().length;individual++){
-        var digit = index.toString()[individual];
-        if (digit === "3"){
-          returnDisplay.push("I'm sorry, Dave. I'm afraid I can't do that.");
-        }else if (digit === "2") {
-          returnDisplay.push("Boop!");
-        }else if (digit === "1") {
-          returnDisplay.push("Beep!");
-        }else {
-          returnDisplay.push(index);
-        }
+      var newString = index.toString();
+      if (newString.includes("3")){
+        returnDisplay.push("I'm sorry, Dave. I'm afraid I can't do that.");
+      }else if (newString.includes("2")) {
+        returnDisplay.push("Boop!");
+      }else if (newString.includes("1")) {
+        returnDisplay.push("Beep!");
+      }else {
+        returnDisplay.push(index);
       }
+
     }
 
     console.log(returnDisplay);
