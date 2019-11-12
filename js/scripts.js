4af3b5f287e2efdfc44e4e8f693f97164970acee
lioness3 <jcnailtech3@yahoo.com>
2019-11-11 16:33:23 -0800

    seperate Ui and BL

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,33 @@
+// Business Logic
+var generateString = function($int) {
+  var returnDisplay = [];
 
-//User Logic
+  for (var index=0;index <= $int;index++){
+    var inputReset = $('input#userInput').val('');
+    var newString = index.toString();
+     if (newString.includes("3")){
+      returnDisplay.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    }else if (newString.includes("2")) {
+      returnDisplay.push("Boop!");
+    }else if (newString.includes("1")) {
+      returnDisplay.push("Beep!");
+    } else{
+      returnDisplay.push(index);
+    };
+  };
+  return returnDisplay.join(", ");
+}
 
+// User Logic.
 $(document).ready(function(){
   $(".form-group").submit(function(event){
     event.preventDefault();
 
+    $("#visualDisplay").show();
 
-    $("#visualDisplay").toggle();
+    var inputValue = parseInt($("input#userInput").val());
 
-
-//business Logic
-    var inputValue = $("input#userInput").val();
-    var returnDisplay = [];
-
-    for (var index=0;index <= parseInt(inputValue);index++){
-      var inputReset = $('input#userInput').val('');
-      var newString = index.toString();
-       if (newString.includes("3")){
-        returnDisplay.push("I'm sorry, Dave. I'm afraid I can't do that.");
-      }else if (newString.includes("2")) {
-        returnDisplay.push("Boop!");
-      }else if (newString.includes("1")) {
-        returnDisplay.push("Beep!");
-      } else{
-        returnDisplay.push(index);
-      };
-    };
-//Clears input line
-    inputReset;
-//joins phrases in the new string and returns it to the user display
-    document.getElementById("visual").innerHTML = returnDisplay.join(", ");
+    //Get joined phrases in the new string and returns it to the user display.
+    document.getElementById("visual").innerHTML = generateString(inputValue);
   });
 });
